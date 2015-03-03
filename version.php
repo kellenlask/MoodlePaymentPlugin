6394209699afce1a97be010018c4626228e868da
Kellen Lask <kellenlask@gmail.com>
2015-03-02 19:43:15 -0700

    Created the Repo
    Added a /db directory and a version.php file

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+$plugin->version   = 2015030200; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014051200; // Requires this Moodle version
+$plugin->cron      = 0; // Period for cron to check this module (secs)
+$plugin->component = 'mod_certificate';
+
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = "Stable"; // User-friendly version number
