c0d6134f1f19f71505c2a472151a3d5dd6f71dad
Kellen Lask <kellenlask@gmail.com>
2015-03-24 16:24:38 -0600

    install.xml
    Added an empty install.xml

--- version.php
+++ version.php
@@ -1,9 +1,21 @@
 <?php
-
+/**
+ * @package   auth_payment
+ * @copyright 2014, You Name <your@email.address>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+defined('MOODLE_INTERNAL') || die();
+ 
 $plugin->version   = 2015030200; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200; // Requires this Moodle version
 $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_certificate';
-
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "Stable"; // User-friendly version number
+$plugin->release   = 'Stable';
+ 
+/*$plugin->dependencies = array(
+    'mod_forum' => ANY_VERSION,
+    'mod_data'  => TODO
+*/
+);
