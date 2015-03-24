7f1ecfc0d5d0e1c659dd7304ba5d7b4eb036f6fa
Kellen Lask <kellenlask@gmail.com>
2015-03-24 17:16:06 -0600

    Discount block
    Started building a discount code block for the admins to use.
    NOTE: This may need to be moved to a new repository or something. Not
    sure yet...

--- discount/block_discount.php
+++ discount/block_discount.php
@@ -1,4 +1,22 @@
 <?php
 
+class block_discount extends block_base {
 
-);
+    public function init() {
+        $this->title = get_string('pluginname', 'block_discount'); //This should grab the app name from the /lang/en/block_discount.php file
+        
+    } //End  public function init()
+
+    public function get_content() {
+        if($this->content !== null) {
+            return $this->content;  //If the content is already configured, skip the rest
+        }
+        
+        $this->content = new stdClass;
+        $this->content->text = 'content';  //ToDo: add content 
+        $this->content->footer = 'Footer'; //ToDo: add Footer
+        
+        return $this->content;
+        
+    } //End public function get_content()    
+} //End Class
---
+++ discount/lang/en/block_discount.php
@@ -0,0 +1,5 @@
+<?php
+
+$string['pluginname'] = 'Discount Code Manager';
+$string['discount'] = 'Discount Code Manager';
+$string['pluginname'] = 'Discount Code Manager';
