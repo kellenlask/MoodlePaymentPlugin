0bd9d47630fd8db2349369da2985629ce014dd99
Kellen Lask <kellenlask@gmail.com>
2015-03-25 11:14:10 -0600

    Transactions Reports
    Admin Reports are actually deprecated (2.2), switched over to a standard
    report that requires login.

--- transactions/index.php
+++ transactions/index.php
@@ -1,9 +1,9 @@
 <?php
 
 //Required Libraries
-require_once(dirname(__FILE__) . '/../config.php');
-require_once($CFG->libdir . '/adminlib.php');
-require_once($CFG->libdir . '/tablelib.php');
+require('../../config.php');
+require_once($CFG->dirroot.'/report/stats/locallib.php');
+require_once($CFG->libdir.'/adminlib.php');
 
 //Page Definitions
 define('DEFAULT_PAGE_SIZE', 20);
@@ -18,8 +18,20 @@ define('SHOW_ALL_PAGE_SIZE', 5000);
 //$page       = optional_param('page', 0, PARAM_INT);                     // which page to show
 //$perpage    = optional_param('perpage', DEFAULT_PAGE_SIZE, PARAM_INT);  // how many per page
 //$currentgroup = optional_param('group', null, PARAM_INT); // Get the active group.
+
+//Require Admin Login
+require_login();
+
 //The page's URL
 $url = new moodle_url('/admin/report/transactions/index.php', array('id' => $id));
+
+//Depending on how demanding the reporting is, this may need to be in effect.
+//raise_memory_limit(MEMORY_EXTRA);
+//core_php_time_limit::raise();
+
+
+
+
 
 //ToDo: complete this method: the code is generic from the internet -- needs to be adapted
 //Given a start date, an end date and a file location, dump the transaction history into the .csv
@@ -34,6 +46,7 @@ function make_csv() {
             $num++;
         }
     }
+    
     $output = fopen("php://output", 'w') or die("Can't open php://output");
     header("Content-Type:application/csv");
     header("Content-Disposition:attachment;filename=pressurecsv.csv");
