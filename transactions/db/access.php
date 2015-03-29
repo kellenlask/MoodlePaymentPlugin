30368e11c2b3934f53fb270043784c313432cb1a
Kellen Lask <kellenlask@gmail.com>
2015-03-29 12:15:41 -0600

    Prices & Transactions
    More messing around with access.php

--- transactions/db/access.php
+++ transactions/db/access.php
@@ -2,12 +2,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-    'report/performance:view' => array(
+    'report/transactions:view' => array(
         'riskbitmask' => RISK_CONFIG,
         'captype' => 'read',
         'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
-            'manager' => CAP_ALLOW
+            'administrator' => CAP_ALLOW
         ),
     )
 );
