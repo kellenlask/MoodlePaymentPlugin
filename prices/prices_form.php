d740603659f887ccf9a0fbc5c062352c58469501
Kellen Lask <kellenlask@gmail.com>
2015-03-29 09:07:43 -0600

    Prices
    Created prices_form.php

---
+++ prices/prices_form.php
@@ -0,0 +1,19 @@
+<?php
+
+class prices_form extends moodleform {
+
+    public function definition() {
+        
+    }
+
+//End public function definition() 
+
+    public function validation($data, $files) {
+        
+    }
+
+//End public function validation($data, $files)
+	return array();
+}
+
+?>
