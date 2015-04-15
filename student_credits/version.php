84abe2901f033b63369effdce36822e2b86bb85f
Kellen Lask <kellenlask@gmail.com>
2015-04-14 21:20:03 -0600

    Credits
    version.php included

---
+++ student_credits/version.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @package   student_credits
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 2015041401; // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2014051200; // Requires this Moodle version
+$plugin->cron = 0; // Period for cron to check this module (secs)
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'Stable';
+
+?>
